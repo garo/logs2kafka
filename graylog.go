b3adad918b3b1037cb95c86e97fb401a02db7d2c
Juho Mäkinen <juho.makinen@gmail.com>
2017-05-24 13:13:21 +0300

    Feature/graylog (#3)
    * Initial version on graylog handing. work in progress
    
    * Graylog message expiration
    
    * More tests for graylog
    
    * Update README.md
    
    * Implement tag key-value handling and json payload as short_message
    
    * Try to use govendor
    
    * Prepare jenkins
    
    * Share same messages channel between different receivers
    
    * Handle graylog edge cases
    
     - Empty short_message property
     - short_message with invalid json

---
+++ graylog.go
@@ -0,0 +1,297 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+	"github.com/Jeffail/gabs"
+)
+
+type Chunk struct {
+	// Total number of chunks to be expected
+	TotalCount int
+	ReceivedCount int
+	ReceivedBytes int
+	Parts [][]byte
+
+	Expiration int64
+
+}
+
+type Graylog struct {
+	Port int
+
+	Messages chan Message
+
+	close chan bool
+
+	Statsd StatisticsSender
+
+	ReceivedChunks map[string]*Chunk
+
+	LastCleanup int64
+}
+
+func (s *Graylog) RunCleanup() error {
+	now := time.Now().UnixNano()
+	for k, v := range s.ReceivedChunks {
+		if now > v.Expiration {
+			delete(s.ReceivedChunks, k)
+		}
+	}
+
+	return nil
+}
+
+func (s *Graylog) HandleChunkedPacket(buffer []byte) error {
+
+	message_id := string(buffer[2:10])
+
+	c, found := s.ReceivedChunks[message_id]
+	if !found {
+		c = &Chunk{}
+		// buffer[11] is Sequence count - 1 byte: Total number of chunks this message has.
+		c.TotalCount = int(buffer[11])
+		c.Parts = make([][]byte, c.TotalCount)
+
+		// Mark expiration 5 seconds into the future
+		c.Expiration = time.Now().UnixNano() + 5e9
+	}
+
+	// buffer[10] is Sequence number - 1 byte:
+	// The sequence number of this chunk. Starting at 0 and always less than the sequence count.
+	c.Parts[int(buffer[10])] = buffer[12:]
+
+	c.ReceivedBytes += len(c.Parts[c.ReceivedCount])
+	c.ReceivedCount += 1
+	
+	s.ReceivedChunks[message_id] = c
+
+	if c.ReceivedCount == c.TotalCount {
+
+		buf := make([]byte, c.ReceivedBytes)
+		cursor := 0
+		for _, sub := range c.Parts {
+			l := len(sub)
+			copy(buf[cursor:cursor+l], sub)
+			cursor += l
+		}
+
+		m := Message{}
+		m.Data = buf
+		err := m.ParseJSON()
+		if err != nil {
+			return err
+		}
+
+		ConvertGraylogFields(&m)
+		delete(s.ReceivedChunks, message_id)
+		s.Messages <- m
+	}
+
+	if s.LastCleanup == 0 || time.Now().UnixNano() > s.LastCleanup + 5e9 {
+		s.LastCleanup = time.Now().UnixNano()
+		s.RunCleanup()
+	}
+
+	return nil
+}
+
+
+func (s *Graylog) ParseGraylogMessage(buffer []byte) (error) {
+	m := Message{}
+
+	if buffer[0] == '{' {
+		// Non-chunked delivery
+		m.Data = buffer
+
+		err := m.ParseJSON()
+		if err != nil {
+			return err
+		}
+
+		ConvertGraylogFields(&m)
+
+		s.Messages <- m
+	} else if buffer[0] == 0x1E && buffer[1] == 0x0F {
+		// Chunked delivery
+		err := s.HandleChunkedPacket(buffer)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+
+
+func (s *Graylog) Init(port int) error {
+	s.Port = port
+	s.close = make(chan bool)
+
+	ServerAddr, err := net.ResolveUDPAddr("udp", ":"+strconv.Itoa(port))
+	if err != nil {
+		return err
+	}
+
+	ServerConn, err := net.ListenUDP("udp", ServerAddr)
+	if err != nil {
+		return err
+	}
+
+	buf := make([]byte, 9500)
+
+	go func() {
+		for {
+			select {
+			case val, ok := <-s.close:
+				if val == true || ok == false {
+					return
+				}
+				break
+			default:
+
+				ServerConn.SetDeadline(time.Now().Add(time.Millisecond * 100))
+				n, _, err := ServerConn.ReadFromUDP(buf)
+
+				if s.Messages != nil && err == nil {
+
+					err := s.ParseGraylogMessage(buf[0:n])
+					if err != nil {
+						if s.Statsd != nil {
+							s.Statsd.Inc("logs2kafka.invalid_graylog_messages", 1, 0.1)
+						}
+						fmt.Fprintf(os.Stderr, "Error parsing json message: %s\n", err)
+					}
+				}
+			}
+		}
+	}()
+
+	return nil
+
+}
+
+func (s *Graylog) Close() {
+	s.close <- true
+}
+
+
+/*
+
+{
+  \"version\": \"1.1\",
+  \"host\": \"delivery-staging-us-east-1b-master-0\",
+  \"short_message\": \"moi\r\",
+  \"timestamp\": 1495112212.5,
+  \"level\": 6,
+  \"_app\": \"test\",
+  \"_command\": \"bash\",
+  \"_container_id\": \"1f7665c78d21073a0c5d58bc5ff6155ee2cce4f4069c234c57ed9bd7e048cb7f\",
+  \"_container_name\": \"berserk_golick\",
+  \"_created\": \"2017-05-18T12:56:45.918230564Z\",
+  \"_image_id\": \"sha256:ebcd9d4fca80e9e8afc525d8a38e7c56825dfb4a220ed77156f9fb13b14d4ab7\",
+  \"_image_name\": \"ubuntu:16.04\",
+  \"_tag\": \"test\"
+}
+
+_container_id
+_image_name
+
+label:
+_app
+io.kubernetes.pod.name
+io.kubernetes.container.name (use this as the topic name)
+*/
+
+func ConvertGraylogFields(m *Message) error {
+
+	container_name, ok := m.Container.Path("_container_name").Data().(string)
+
+	if ok {
+		m.Container.Set(container_name, "container_name")
+		m.Container.Delete("_container_name")
+	}
+
+	level_number, ok := m.Container.Path("level").Data().(float64)
+	if ok {
+		m.Container.Delete("level")
+		switch level_number {
+			case 3:
+				m.Container.Set("ERROR", "level")
+			case 4:
+				m.Container.Set("WARN", "level")
+			case 6:
+				m.Container.Set("INFO", "level")
+			case 7:
+				m.Container.Set("DEBUG", "level")
+			default:
+				m.Container.Set("UNKNOWN", "level")
+		}
+	}
+
+	// We just drop the float timestamp and generate our own ts field later
+	m.Container.Delete("timestamp")
+
+	short_message, ok := m.Container.Path("short_message").Data().(string)
+	if ok {
+
+		// Check if the short_message is in fact a JSON document and handle that
+		if len(short_message) > 1 && short_message[0] == '{' {
+			short_message_data := m.Container.Path("short_message").Data().(string)
+			parsed_json_message, err := gabs.ParseJSON([]byte(short_message_data))
+
+			if err != nil {
+				// Error in JSON parse, just pass the data through as-is
+				m.Container.Set(short_message, "msg")
+			} else {
+
+				children, _ := parsed_json_message.ChildrenMap()
+				for key, value := range children {
+					_, err = m.Container.Set(value.Data(), key)
+				}
+			}
+		} else {
+			m.Container.Set(short_message, "msg")
+		}
+
+		m.Container.Delete("short_message")
+	}
+
+	// Process tag. We can extract json properties out with the "key1=value1,key2=value2" notation
+	tag, ok := m.Container.Path("_tag").Data().(string)
+	if ok {
+		parts := strings.Split(tag, ",")
+		replaced := false
+		for _, part := range parts {
+			keyvalue := strings.Split(part, "=")
+			if len(keyvalue) == 2 {
+				fmt.Printf("Found key %s to value %s\n", keyvalue[0], keyvalue[1])
+				m.Container.Set(keyvalue[1], keyvalue[0])
+				replaced = true
+			}
+		}
+
+		// Delete the _tag if we did parse things out from it
+		if replaced {
+			m.Container.Delete("_tag")
+		}
+	}
+
+	// Convert registry2.applifier.info:5005/comet-source-adapter@sha256:f205ed11f1a26bb8ceefc9389ebe6
+	// to registry2.applifier.info:5005/comet-source-adapter:f205ed11f1a26bb8ceefc9389ebe6
+	// But only if there is no docker_image already set as this could be passed from tag handling
+	_, ok = m.Container.Path("docker_image").Data().(string)
+	if !ok {
+		image_name, ok := m.Container.Path("_image_name").Data().(string)
+		if ok {
+			m.Container.Set(image_name, "docker_image")
+		}
+	}
+
+	return nil
+}
---
+++ graylog_test.go
@@ -0,0 +1,400 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"net"
+	"time"
+)
+
+func TestGraylogMessageConversion(t *testing.T) {
+
+	str := `{"version":"1.1",
+	"host":"delivery-staging-us-east-1b-master-0",
+	"short_message":"moi\r",
+	"timestamp":1.495174443806e+09,
+	"level":3,
+	"_command":"/hyperkube controller-manager --master=http://127.0.0.1:8080 --service-account-private-key-file=/etc/kubernetes/ssl/client-server-key.pem --root-ca-file=/etc/kubernetes/ssl/ca.pem --leader-elect=true --cloud-provider=aws",
+	"_container_id":"e02acf37f963ec2d46ede21766070559aa4a79c1afc8582e27d580ef2326800e",
+	"_container_name":"k8s_kube-controller-manager_kube-controller-manager-ip-172-16-1-150.ec2.internal_kube-system_b50f4a11f4401d857fc4bbb355fceae8_0",
+	"_created":"2017-05-19T06:14:02.196020749Z",
+	"_image_id":"sha256:91a48ff795cebb88646ec6cc4955bf7f500466feb44ec18b508f2ce94ed0d9b7",
+	"_image_name":"quay.io/coreos/hyperkube@sha256:77b81b118e6e231d284e6ae0ec50d898dadd88af469df33d5cf3f3a2d0d44473",
+	"_io.kubernetes.container.name":"kube-controller-manager",
+	"_io.kubernetes.pod.name":"kube-controller-manager-ip-172-16-1-150.ec2.internal",
+	"_tag":"test"}`
+	m := JSONToMessage(str)
+	err := m.ParseJSON()
+	assert.Nil(t, err)
+
+	ConvertGraylogFields(&m)
+
+	value, ok := m.Container.Path("msg").Data().(string)
+	assert.Equal(t, ok, true)
+	assert.Equal(t, value, "moi\r")
+
+	value, ok = m.Container.Path("level").Data().(string)
+	assert.Equal(t, ok, true)
+	assert.Equal(t, value, "ERROR")
+
+}
+
+func TestGraylogEmptyMessage(t *testing.T) {
+
+	str := `{"short_message":""}`
+	m := JSONToMessage(str)
+	err := m.ParseJSON()
+	assert.Nil(t, err)
+
+	ConvertGraylogFields(&m)
+
+	value, ok := m.Container.Path("msg").Data().(string)
+	assert.Equal(t, ok, true)
+	assert.Equal(t, value, "")
+}
+
+func TestGraylogCorruptedMessage(t *testing.T) {
+
+	str := `{"short_message":"{\"", "foo": "bar"}`
+	m := JSONToMessage(str)
+	err := m.ParseJSON()
+	assert.Nil(t, err)
+
+	err = ConvertGraylogFields(&m)
+	assert.Equal(t, err, nil)
+	
+
+	value, ok := m.Container.Path("foo").Data().(string)
+	assert.Equal(t, ok, true)
+	assert.Equal(t, value, "bar")
+
+	value, ok = m.Container.Path("msg").Data().(string)
+	assert.Equal(t, ok, true)
+	assert.Equal(t, value, "{\"")
+
+}
+
+func TestGraylogMessageConversionForPodMessage(t *testing.T) {
+
+	str := `
+	{"version":"1.1",
+	"host":"delivery-staging-us-east-1b-asg-general",
+	"short_message":"main.main()",
+	"timestamp":1.495173922171e+09,
+	"level":7,
+	"_command":"/comet-source-adapter","_container_id":"de57c44274f63c39455041c9515c0e12b856bef6533f8e4f8b3489852a0d7208",
+	"_container_name":"k8s_ads-auction-comet-source-adapter_ads-auction-835331642-rzzgr_default_99baeb50-3bc3-11e7-a061-0a50dcb4a89e_1",
+	"_created":"2017-05-19T06:05:22.037120689Z",
+	"_image_id":"sha256:321a61fdf20848467b4eedc1f8d113308785eccefa21d2014b96d48021cbb309",
+	"_image_name":"registry2.applifier.info:5005/comet-source-adapter@sha256:f205ed11f1a26bbfe3dd127adcf155949b9fb205b19821c8b78ceefc9389ebe6",
+	"_io.kubernetes.container.name":"ads-auction-comet-source-adapter",
+	"_io.kubernetes.pod.name":"ads-auction-835331642-rzzgr",
+	"_tag":"test"
+	}`
+
+	m := JSONToMessage(str)
+	err := m.ParseJSON()
+	assert.Nil(t, err)
+
+	ConvertGraylogFields(&m)
+
+	value, ok := m.Container.Path("container_name").Data().(string)
+	assert.Equal(t, ok, true)
+	assert.Equal(t, value, "k8s_ads-auction-comet-source-adapter_ads-auction-835331642-rzzgr_default_99baeb50-3bc3-11e7-a061-0a50dcb4a89e_1")
+
+	value, ok = m.Container.Path("level").Data().(string)
+	assert.Equal(t, ok, true)
+	assert.Equal(t, value, "DEBUG")
+
+	value, ok = m.Container.Path("docker_image").Data().(string)
+	assert.Equal(t, ok, true)
+	assert.Equal(t, value, "registry2.applifier.info:5005/comet-source-adapter@sha256:f205ed11f1a26bbfe3dd127adcf155949b9fb205b19821c8b78ceefc9389ebe6")
+
+}
+
+func TestGraylogMessageJSONin_short_message_field(t *testing.T) {
+
+	str := `
+	{"version":"1.1",
+	"host":"delivery-staging-us-east-1b-asg-general",
+	"short_message":"{\"msg\":\"hei\",\"foo\":\"bar\",\"obj\":{\"hello\":\"world\"},\"arr\":[10,20,30]}\r",
+	"timestamp":1.495516067312e+09,
+	"level":6,
+	"_container_id":"82331064bdf5fb786916d5a073269a1b454b2f27112f2c091ed820d834ebe977",
+	"_container_name":"mycontainer","_created":"2017-05-23T05:07:47.079689035Z",
+	"_image_id":"sha256:ebcd9d4fca80e9e8afc525d8a38e7c56825dfb4a220ed77156f9fb13b14d4ab7",
+	"_image_name":"ubuntu:16.04","_tag":"test"
+	}`
+
+	m := JSONToMessage(str)
+	err := m.ParseJSON()
+	assert.Nil(t, err)
+
+	ConvertGraylogFields(&m)
+
+	value, ok := m.Container.Path("msg").Data().(string)
+	assert.Equal(t, ok, true)
+	assert.Equal(t, value, "hei")
+
+	value, ok = m.Container.Path("short_message").Data().(string)
+	assert.Equal(t, ok, false)
+
+	value, ok = m.Container.Path("foo").Data().(string)
+	assert.Equal(t, ok, true)
+	assert.Equal(t, value, "bar")
+
+	value, ok = m.Container.Path("obj.hello").Data().(string)
+	assert.Equal(t, ok, true)
+	assert.Equal(t, value, "world")
+
+	assert.Equal(t, 10, m.Container.S("arr").Index(0).Data().(float64))
+	assert.Equal(t, 20, m.Container.S("arr").Index(1).Data().(float64))
+	assert.Equal(t, 30, m.Container.S("arr").Index(2).Data().(float64))
+
+}
+
+func TestGraylogMessageTagFieldExtraction(t *testing.T) {
+
+	str := `{"version":"1.1",
+	"host":"delivery-staging-us-east-1b-master-0",
+	"short_message":"moi\r",
+	"timestamp":1.495174443806e+09,
+	"level":3,
+	"_command":"/hyperkube controller-manager --master=http://127.0.0.1:8080 --service-account-private-key-file=/etc/kubernetes/ssl/client-server-key.pem --root-ca-file=/etc/kubernetes/ssl/ca.pem --leader-elect=true --cloud-provider=aws",
+	"_container_id":"e02acf37f963ec2d46ede21766070559aa4a79c1afc8582e27d580ef2326800e",
+	"_container_name":"k8s_kube-controller-manager_kube-controller-manager-ip-172-16-1-150.ec2.internal_kube-system_b50f4a11f4401d857fc4bbb355fceae8_0",
+	"_created":"2017-05-19T06:14:02.196020749Z",
+	"_image_id":"sha256:91a48ff795cebb88646ec6cc4955bf7f500466feb44ec18b508f2ce94ed0d9b7",
+	"_image_name":"quay.io/coreos/hyperkube@sha256:77b81b118e6e231d284e6ae0ec50d898dadd88af469df33d5cf3f3a2d0d44473",
+	"_io.kubernetes.container.name":"kube-controller-manager",
+	"_io.kubernetes.pod.name":"kube-controller-manager-ip-172-16-1-150.ec2.internal",
+	"_tag":"docker_image=registry.applifier.info:5000/kafka:0.8.2.1,foo=bar"}`
+	m := JSONToMessage(str)
+	err := m.ParseJSON()
+	assert.Nil(t, err)
+
+	ConvertGraylogFields(&m)
+
+	value, ok := m.Container.Path("docker_image").Data().(string)
+	assert.Equal(t, ok, true)
+	assert.Equal(t, value, "registry.applifier.info:5000/kafka:0.8.2.1")
+
+	value, ok = m.Container.Path("_image_name").Data().(string)
+	assert.Equal(t, ok, true)
+	assert.Equal(t, value, "quay.io/coreos/hyperkube@sha256:77b81b118e6e231d284e6ae0ec50d898dadd88af469df33d5cf3f3a2d0d44473")
+
+	value, ok = m.Container.Path("foo").Data().(string)
+	assert.Equal(t, ok, true)
+	assert.Equal(t, value, "bar")
+
+}
+
+func TestGraylogReceive(t *testing.T) {
+
+	s := Graylog{}
+
+	s.Messages = make(chan Message)
+
+	err := s.Init(9998)
+	assert.Nil(t, err)
+	assert.Equal(t, 9998, s.Port)
+
+	ServerAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:9998")
+	assert.Nil(t, err)
+	LocalAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
+	assert.Nil(t, err)
+
+	Conn, err := net.DialUDP("udp", LocalAddr, ServerAddr)
+	assert.Nil(t, err)
+
+	defer Conn.Close()
+	_, err = Conn.Write([]byte(`{"version":"1.1","host":"delivery-staging-us-east-1b-asg-general","short_message":"main.main()","timestamp":1.495173922171e+09,"level":3,"_command":"/comet-source-adapter","_container_id":"de57c44274f63c39455041c9515c0e12b856bef6533f8e4f8b3489852a0d7208","_container_name":"k8s_ads-auction-comet-source-adapter_ads-auction-835331642-rzzgr_default_99baeb50-3bc3-11e7-a061-0a50dcb4a89e_1","_created":"2017-05-19T06:05:22.037120689Z","_image_id":"sha256:321a61fdf20848467b4eedc1f8d113308785eccefa21d2014b96d48021cbb309","_image_name":"registry2.applifier.info:5005/comet-source-adapter@sha256:f205ed11f1a26bbfe3dd127adcf155949b9fb205b19821c8b78ceefc9389ebe6","_io.kubernetes.container.name":"ads-auction-comet-source-adapter","_io.kubernetes.pod.name":"ads-auction-835331642-rzzgr","_tag":"test"}`))
+	assert.Nil(t, err)
+	Conn.Close()
+
+	msg := <-s.Messages
+	value, ok := msg.Container.Path("msg").Data().(string)
+	assert.Equal(t, true, ok)
+	assert.Equal(t, "main.main()", value)
+
+	s.Close()
+
+}
+
+func TestGraylogChunkProcessSingle(t *testing.T) {
+
+	s := Graylog{}
+
+	s.ReceivedChunks = make(map[string]*Chunk)
+
+	s.HandleChunkedPacket([]byte("\x1e\x0f\x00\x00\x00\x00\xDE\xAD\xBE\xEF\x00\x02{\"version\":\"1.1\",\"host\":\"delivery-staging-us-east-1b-asg-general\",\"test\":\"foobar\""))
+
+	chunk, found := s.ReceivedChunks["\x00\x00\x00\x00\xDE\xAD\xBE\xEF"]
+	assert.Equal(t, true, found)
+	assert.Equal(t, 1, chunk.ReceivedCount)
+	assert.Equal(t, 2, chunk.TotalCount)
+	assert.Equal(t, byte('{'), chunk.Parts[0][0])
+
+}
+
+func TestGraylogChunkProcessSingle2(t *testing.T) {
+
+	s := Graylog{}
+
+	s.ReceivedChunks = make(map[string]*Chunk)
+
+	s.HandleChunkedPacket([]byte("\x1e\x0f\x00\x00\x00\x00\xDE\xAD\xBE\xEF\x01\x02,\"test2\":\"hello\"}"))
+
+	chunk, found := s.ReceivedChunks["\x00\x00\x00\x00\xDE\xAD\xBE\xEF"]
+	assert.Equal(t, true, found)
+	assert.Equal(t, 1, chunk.ReceivedCount)
+	assert.Equal(t, 2, chunk.TotalCount)
+	assert.Equal(t, byte(','), chunk.Parts[1][0])
+	assert.Equal(t, byte('"'), chunk.Parts[1][1])
+}
+
+func TestGraylogChunkProcessComplete(t *testing.T) {
+
+	s := Graylog{}
+
+	s.ReceivedChunks = make(map[string]*Chunk)
+	s.Messages = make(chan Message, 10)
+
+	s.HandleChunkedPacket([]byte("\x1e\x0f\x00\x00\x00\x00\xDE\xAD\xBE\xEF\x00\x02{\"version\":\"1.1\",\"host\":\"delivery-staging-us-east-1b-asg-general\",\"test\":\"foobar\",\"level\":7"))
+
+	assert.Equal(t, 1, len(s.ReceivedChunks))
+	
+	s.HandleChunkedPacket([]byte("\x1e\x0f\x00\x00\x00\x00\xDE\xAD\xBE\xEF\x01\x02,\"test2\":\"hello\"}"))
+
+	msg := <-s.Messages
+	value, ok := msg.Container.Path("test").Data().(string)
+	assert.Equal(t, true, ok)
+	assert.Equal(t, "foobar", value)
+
+	value, ok = msg.Container.Path("test2").Data().(string)
+	assert.Equal(t, true, ok)
+	assert.Equal(t, "hello", value)
+
+	// Ensure that ConvertGraylogFields has been called
+	value, ok = msg.Container.Path("level").Data().(string)
+	assert.Equal(t, true, ok)
+	assert.Equal(t, "DEBUG", value)
+
+	assert.Equal(t, 0, len(s.ReceivedChunks))
+
+}
+
+func TestGraylogSingleFullChunkProcessing(t *testing.T) {
+
+	s := Graylog{}
+
+	s.ReceivedChunks = make(map[string]*Chunk)
+	s.Messages = make(chan Message, 10)
+
+	s.HandleChunkedPacket([]byte("\x1e\x0f\x00\x00\x00\x00\xDE\xAD\xBE\xEF\x00\x01{\"version\":\"1.1\",\"host\":\"delivery-staging-us-east-1b-asg-general\",\"test\":\"foobar\",\"level\":7}"))
+
+	msg := <-s.Messages
+	value, ok := msg.Container.Path("test").Data().(string)
+	assert.Equal(t, true, ok)
+	assert.Equal(t, "foobar", value)
+
+	assert.Equal(t, 0, len(s.ReceivedChunks))
+
+}
+
+func TestGraylogCleanupWorks(t *testing.T) {
+
+	s := Graylog{}
+
+	s.ReceivedChunks = make(map[string]*Chunk)
+	s.Messages = make(chan Message, 10)
+
+	s.HandleChunkedPacket([]byte("\x1e\x0f\x00\x00\x00\x00\xDE\xAD\xBE\xEF\x00\x02{\"version\":\"1.1\",\"host\":\"delivery-staging-us-east-1b-asg-general\",\"test\":\"foobar\""))
+
+	assert.Equal(t, 1, len(s.ReceivedChunks))
+	s.RunCleanup()
+	assert.Equal(t, 1, len(s.ReceivedChunks))
+
+	chunk := s.ReceivedChunks["\x00\x00\x00\x00\xDE\xAD\xBE\xEF"]
+	chunk.Expiration -= 6e9 // 6 seconds
+	s.RunCleanup()
+	assert.Equal(t, 0, len(s.ReceivedChunks))
+
+}
+
+
+func TestGraylogRunCleanupEvery5Seconds(t *testing.T) {
+
+	s := Graylog{}
+
+	s.ReceivedChunks = make(map[string]*Chunk)
+	s.Messages = make(chan Message, 10)
+	s.LastCleanup = time.Now().UnixNano()
+
+	s.HandleChunkedPacket([]byte("\x1e\x0f\x00\x00\x00\x00\xDE\xAD\xBE\xEF\x00\x02{\"version\":\"1.1\",\"host\":\"delivery-staging-us-east-1b-asg-general\",\"test\":\"foobar\""))
+
+	// Simulate that six seconds have passed
+	chunk := s.ReceivedChunks["\x00\x00\x00\x00\xDE\xAD\xBE\xEF"]
+	chunk.Expiration -= 6e9
+
+	// A call to HandleChunkedPacket should trigger RunCleanup if previous
+	// was done over 5 seconds ago.
+	//
+	// But since LastCleanup is not changed sending another packet 
+	// not trigger any cleanup
+	s.HandleChunkedPacket([]byte("\x1e\x0f\x00\x00\x00\x00\xDE\xAD\x00\x00\x00\x02{\"version\":\"1.1\",\"host\":\"delivery-staging-us-east-1b-asg-general\",\"test\":\"foobar\""))
+	assert.Equal(t, 2, len(s.ReceivedChunks))
+
+	// After this the next HandleChunkedPacket should trigger cleanup
+	s.LastCleanup -= 6e9
+
+	s.HandleChunkedPacket([]byte("\x1e\x0f\x00\x00\x00\x00\x00\xAD\xBE\xEF\x00\x02{\"version\":\"1.1\",\"host\":\"delivery-staging-us-east-1b-asg-general\",\"test\":\"foobar\""))
+
+	assert.Equal(t, 2, len(s.ReceivedChunks))
+
+}
+
+/*
+func TestGraylogFullChunkReceive(t *testing.T) {
+
+	s := Graylog{}
+
+	s.Messages = make(chan Message)
+
+	err := s.Init(9997)
+	assert.Nil(t, err)
+	assert.Equal(t, 9997, s.Port)
+
+	ServerAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:9997")
+	assert.Nil(t, err)
+	LocalAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
+	assert.Nil(t, err)
+
+	Conn, err := net.DialUDP("udp", LocalAddr, ServerAddr)
+	assert.Nil(t, err)
+
+	defer Conn.Close()
+	_, err = Conn.Write([]byte("\x1e\x0f\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02{\"version\":\"1.1\",\"host\":\"delivery-staging-us-east-1b-asg-general\",\"test\":\"foobar\""))
+	
+	_, err = Conn.Write([]byte("\x1e\x0f\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x02,\"short_message\":\"main.main()\",\"timestamp\":1.495173922171e+09,\"level\":3,\"_command\":\"/comet-source-adapter\",\"_container_id\":\"de57c44274f63c39455041c9515c0e12b856bef6533f8e4f8b3489852a0d7208\",\"_container_name\":\"k8s_ads-auction-comet-source-adapter_ads-auction-835331642-rzzgr_default_99baeb50-3bc3-11e7-a061-0a50dcb4a89e_1\",\"_created\":\"2017-05-19T06:05:22.037120689Z\",\"_image_id\":\"sha256:321a61fdf20848467b4eedc1f8d113308785eccefa21d2014b96d48021cbb309\",\"_image_name\":\"registry2.applifier.info:5005/comet-source-adapter@sha256:f205ed11f1a26bbfe3dd127adcf155949b9fb205b19821c8b78ceefc9389ebe6\",\"_io.kubernetes.container.name\":\"ads-auction-comet-source-adapter\",\"_io.kubernetes.pod.name\":\"ads-auction-835331642-rzzgr\",\"_tag\":\"test\"}"))
+	assert.Nil(t, err)
+	Conn.Close()
+
+	msg := <-s.Messages
+	value, ok := msg.Container.Path("msg").Data().(string)
+	assert.Equal(t, true, ok)
+	assert.Equal(t, "main.main()", value)
+
+	value, ok = msg.Container.Path("version").Data().(string)
+	assert.Equal(t, true, ok)
+	assert.Equal(t, "1.1", value)
+
+	value, ok = msg.Container.Path("_command").Data().(string)
+	assert.Equal(t, true, ok)
+	assert.Equal(t, "/comet-source-adapter", value)
+
+
+	s.Close()
+
+}
+*/
